8d227bbf6a3f14cf3fd5b0ad770718da1ebce6f6
BonnieAxelsen <104758739+BonnieAxelsen@users.noreply.github.com>
2022-11-03 12:49:11 +0100

    Removed empty href link

--- src/Footer.js
+++ src/Footer.js
@@ -4,12 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="Footer">
-      <p>
-        <a href="" target="_blank">
-          Open-source code&nbsp;
-        </a>
-        by Bonnie Hill - hosted on Netlify
-      </p>
+      <p>Open-source code&nbsp; by Bonnie Hill - hosted on Netlify</p>
     </div>
   );
 }
